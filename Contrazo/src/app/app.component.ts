0be7daca412ed97d3e63bf81d45f18529116af0e
Mónica Zamorano <mzamoranos@uoc.edu>
2024-04-14 22:32:06 +0200

    footer

--- Contrazo/src/app/app.component.ts
+++ Contrazo/src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from "./footer/footer.component";
+import { CabeceraComponent } from "./cabecera/cabecera.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.sass'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.sass',
+    imports: [CommonModule, RouterOutlet, FooterComponent, CabeceraComponent]
 })
 export class AppComponent {
   title = 'Contrazo';
